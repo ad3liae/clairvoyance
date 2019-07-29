745b790bf8423f95113ba0452ddfb4f065a60986
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2019-07-29 18:44:58 +0900

    Deadcode cleanups

--- clairvoyance/app/detect.py
+++ clairvoyance/app/detect.py
@@ -2,17 +2,8 @@ import asyncio
 import random
 
 from lipnet.lipreading.videos import Video
-from lipnet.lipreading.visualization import show_video_subtitle
-from lipnet.core.decoders import Decoder
-from lipnet.lipreading.helpers import labels_to_text
-from lipnet.utils.spell import Spell
-from lipnet.model2 import LipNet
-from keras.optimizers import Adam
-import numpy as np
 import sys
 import os
-
-np.random.seed(55)
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 FACE_PREDICTOR_PATH = os.path.join(CURRENT_PATH,'..','..','LipNet','common','predictors','shape_predictor_68_face_landmarks.dat')
@@ -33,12 +24,12 @@ class FaceRecognitionTask:
 
     async def do(self, video_path):
         for i in range(1000):
-            print("Sending batch #{}\n".format(i))
-            print("\nLoading data from disk...")
+            print("Sending batch #{}".format(i))
+            print("Loading data from disk...")
             video = Video(vtype='face', face_predictor_path=FACE_PREDICTOR_PATH)
             if os.path.isfile(video_path):
                 video.from_video(video_path)
             else:
                 video.from_frames(video_path)
-            print("Data loaded.\n")
+            print("Data loaded.")
             await asyncio.get_event_loop().run_in_executor(None, self._q.put, video)
