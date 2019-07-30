9488b42affc7e689851c83706ac469ba3b236abd
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2019-07-31 08:42:20 +0900

    Experimentally interpolating face frames on sporadic face drops

--- LipNet/lipnet/lipreading/videos.py
+++ LipNet/lipnet/lipreading/videos.py
@@ -158,37 +158,41 @@ class Video(object):
             for k, d in enumerate(dets):
                 shape = predictor(frame, d)
                 i = -1
-            if shape is None: # Detector doesn't detect face, just return as is
-                return frames
-            mouth_points = []
-            for part in shape.parts():
-                i += 1
-                if i < 48: # Only take mouth region
-                    continue
-                mouth_points.append((part.x,part.y))
-            np_mouth_points = np.array(mouth_points)
+            if shape is None: # Detector doesn't detect face, interpolate with the last frame
+                try:
+                    mouth_frames.append(mouth_frames[-1])
+                except IndexError:
+                    raise NotImplementedError()
+            else:
+                mouth_points = []
+                for part in shape.parts():
+                    i += 1
+                    if i < 48: # Only take mouth region
+                        continue
+                    mouth_points.append((part.x,part.y))
+                np_mouth_points = np.array(mouth_points)
 
-            mouth_centroid = np.mean(np_mouth_points[:, -2:], axis=0)
+                mouth_centroid = np.mean(np_mouth_points[:, -2:], axis=0)
 
-            if normalize_ratio is None:
-                mouth_left = np.min(np_mouth_points[:, :-1]) * (1.0 - HORIZONTAL_PAD)
-                mouth_right = np.max(np_mouth_points[:, :-1]) * (1.0 + HORIZONTAL_PAD)
+                if normalize_ratio is None:
+                    mouth_left = np.min(np_mouth_points[:, :-1]) * (1.0 - HORIZONTAL_PAD)
+                    mouth_right = np.max(np_mouth_points[:, :-1]) * (1.0 + HORIZONTAL_PAD)
 
-                normalize_ratio = MOUTH_WIDTH / float(mouth_right - mouth_left)
+                    normalize_ratio = MOUTH_WIDTH / float(mouth_right - mouth_left)
 
-            new_img_shape = (int(frame.shape[0] * normalize_ratio), int(frame.shape[1] * normalize_ratio))
-            resized_img = imresize(frame, new_img_shape)
+                new_img_shape = (int(frame.shape[0] * normalize_ratio), int(frame.shape[1] * normalize_ratio))
+                resized_img = imresize(frame, new_img_shape)
 
-            mouth_centroid_norm = mouth_centroid * normalize_ratio
+                mouth_centroid_norm = mouth_centroid * normalize_ratio
 
-            mouth_l = int(mouth_centroid_norm[0] - MOUTH_WIDTH / 2)
-            mouth_r = int(mouth_centroid_norm[0] + MOUTH_WIDTH / 2)
-            mouth_t = int(mouth_centroid_norm[1] - MOUTH_HEIGHT / 2)
-            mouth_b = int(mouth_centroid_norm[1] + MOUTH_HEIGHT / 2)
+                mouth_l = int(mouth_centroid_norm[0] - MOUTH_WIDTH / 2)
+                mouth_r = int(mouth_centroid_norm[0] + MOUTH_WIDTH / 2)
+                mouth_t = int(mouth_centroid_norm[1] - MOUTH_HEIGHT / 2)
+                mouth_b = int(mouth_centroid_norm[1] + MOUTH_HEIGHT / 2)
 
-            mouth_crop_image = resized_img[mouth_t:mouth_b, mouth_l:mouth_r]
+                mouth_crop_image = resized_img[mouth_t:mouth_b, mouth_l:mouth_r]
 
-            mouth_frames.append(mouth_crop_image)
+                mouth_frames.append(mouth_crop_image)
         return mouth_frames
 
     def get_video_frames(self, path):
